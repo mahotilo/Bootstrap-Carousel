dd04131a188528a6d3db0d77be77e22aaa33d8bb
mahotilo <kvmahotilo@gmail.com>
2019-12-03 01:32:09 +0200

    Add files via upload

--- TwitterCarousel.php
+++ TwitterCarousel.php
@@ -8,53 +8,61 @@ class TwitterCarousel{
 
 	/**
 	 * Determine if the $type matches this content key
-	 *
 	 */
 	static function ContentKeyMatch($type){
 		global $addonFolderName;
-
-		if( $addonFolderName === $type ){ //legacy support
-			return true;
-		}
-
 		if( $type === self::content_key ){
 			return true;
 		}
 	}
 
 
-
 	/**
 	 * @static
-	 *
 	 */
+	static function GetHead() {
+		global $page, $addonRelativeCode;
+		$page->head_js[]  = $addonRelativeCode . '/jquery.mobile.custom.js';
+		$page->head_js[]  = $addonRelativeCode . '/carousel.js';
+		if ( $section_data['alt_style'] ){
+			$page->css_user[] = $addonRelativeCode . '/carousel2.css';}
+		else{
+			$page->css_user[] = $addonRelativeCode . '/carousel.css';
+		}
+	}
+
+
 	static function SectionTypes($section_types, $generated_key = false ){
-
-		$section_types[self::content_key] = array('label' => 'Bootstrap Carousel Gallery');
-
+		$section_types[self::content_key] = array();
+		$section_types[self::content_key]['label'] = 'Bootstrap Carousel Gallery';
 		return $section_types;
 	}
 
-	/**
-	 * @static
-	 *
-	 */
-	static function SectionToContent($section_data){
-		global $dataDir;
 
+	static function NewSections($links){
+		global $addonRelativeCode;
+		foreach($links as $key => $section_type_arr){
+			if( $section_type_arr[0] == self::content_key ){
+				$links[$key] = array(self::content_key, $addonRelativeCode . '/icon/section.png');
+			}
+		}
+		return $links;
+	}
+
+
+	static function SectionToContent($section_data){
 		if( !self::ContentKeyMatch($section_data['type']) ){
 			return $section_data;
 		}
-
+		global $dataDir;
 		//update content
 		if( !isset($section_data['content_version']) ){
 			$section_data['content'] = self::GenerateContent($section_data);
 		}
-
 		self::AddComponents();
-
 		return $section_data;
 	}
+
 
 	static function GenerateContent($section_data){
 		global $dataDir;
@@ -82,8 +90,8 @@ class TwitterCarousel{
 			if( empty($caption) ){
 				$caption_class = 'no_caption';
 			}
-			$images .= '<div class="carousel-item '.$class.'">'
-						.'<img src="'.$img.'" class="d-block w-100" alt="'.$caption.'">'
+			$images .= '<div class="item carousel-item '.$class.'">'
+						.'<img src="'.common::GetDir('/include/imgs/blank.gif').'" style="background-image:url('.$img.')" alt="'.$caption.'">'
 						.'<div class="caption carousel-caption '.$caption_class.'">'.$caption.'</div>'
 						.'</div>';
 
@@ -99,7 +107,6 @@ class TwitterCarousel{
 
 		ob_start();
 
-
 		$class = 'gp_twitter_carousel carousel slide';
 		if( !$section_data['auto_start'] ){
 			$class .= ' start_paused';
@@ -108,84 +115,82 @@ class TwitterCarousel{
 		if( isset($section_data['interval_speed']) && is_numeric($section_data['interval_speed']) ){
 			$attr = ' data-speed="'.$section_data['interval_speed'].'"';
 		}
+		$attr .= ' data-pause="hover"';
 		
-		if ($section_data['height'] == '' || $section_data['height'] == '100%') {
+		if ($section_data['height'] == '') {
 			$section_data['height'] = 'auto';
 		}
-		
-		echo '<div id="'.$id.'" class="'.$class.'"'.$attr.' data-ride="carousel" style="height:'.$section_data['height'].'">';
+		echo '<div id="'.$id.'" class="'.$class.'"'.$attr.'>';
+		echo '<div style="padding-bottom:'.$section_data['height'].'">';
 		// Indicators
 		echo '<ol class="carousel-indicators">';
 		echo $indicators;
 		echo '</ol>';
 
 		// Carousel items
-		echo '<div class="carousel-inner" style="height:100%">';
+		echo '<div class="carousel-inner">';
 		echo $images;
 		echo '</div>';
 
 		// Carousel nav
-		echo '<a class="carousel-control-prev" href="#'.$id.'" role="button" data-slide="prev" aria-label="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span></a>';
-		echo '<a class="carousel-control-next" href="#'.$id.'" role="button" data-slide="next" aria-label="next"><span class="carousel-control-next-icon" aria-hidden="true"></span></a>';
-		echo '</div>';
+		echo '<a class="carousel-control left carousel-control-prev" data-target="#'.$id.'" href="#'.$id.'" role="button" data-slide="prev" aria-label="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span></a>';
+		echo '<a class="carousel-control right carousel-control-next" data-target="#'.$id.'" href="#'.$id.'" role="button" data-slide="next" aria-label="next"><span class="carousel-control-next-icon" aria-hidden="true"></span></a>';
+		echo '<span class="gp_blank_img" data-src="'.common::GetDir('/include/imgs/blank.gif').'" style="display:none"></span>';
+		echo '</div></div>';
+
 
 		return ob_get_clean();
 	}
 
 
-
-	/**
-	 * @static
-	 */
 	static function DefaultContent($default_content,$type){
-
 		if( !self::ContentKeyMatch($type) ){
 			return $default_content;
 		}
 
 		$section = array();
 
-
 		ob_start();
 		$id = 'carousel_'.time();
 
-		echo '<div id="'.$id.'" class="gp_twitter_carousel carousel slide" data-ride="carousel">';
+		echo '<div id="'.$id.'" class="gp_twitter_carousel carousel slide">';
+		echo '<div style="padding-bottom:30%">';
 		echo '<ol class="carousel-indicators">';
 		echo '<li class="active gp_to_remove"></li>';
 		echo '</ol>';
 
 		//<!-- Carousel items -->
-		echo '<div class="carousel-inner" style="height:auto">';
-		echo '<div class="carousel-item active gp_to_remove"><img/></div>';
+		echo '<div class="carousel-inner">';
+		echo '<div class="item carousel-item active gp_to_remove"><img/></div>';
 		echo '</div>';
 
 		//<!-- Carousel nav -->
-		echo '<a class="carousel-control-prev" href="#'.$id.'" role="button" data-slide="prev" aria-label="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span></a>';
-		echo '<a class="carousel-control-next" href="#'.$id.'" role="button" data-slide="next" aria-label="next"><span class="carousel-control-next-icon" aria-hidden="true"></span></a>';
-		echo '</div>';
+		echo '<a class="carousel-control left carousel-control-prev" data-target="#'.$id.'" href="#'.$id.'" role="button" data-slide="prev" aria-label="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span></a>';
+		echo '<a class="carousel-control right carousel-control-next" data-target="#'.$id.'" href="#'.$id.'" role="button" data-slide="next" aria-label="next"><span class="carousel-control-next-icon" aria-hidden="true"></span></a>';
+		echo '<span class="gp_blank_img" data-src="'.common::GetDir('/include/imgs/blank.gif').'" style="display:none"></span>';
+		echo '</div></div>';
 
+		$section['gp_label'] = 'Bootstrap Carousel Gallery';
+		$section['gp_color'] = '#8d3ee8';
 		$section['content'] = ob_get_clean();
 		$section['height'] = '30%';
 		$section['auto_start'] = false;
+		$section['alt_style'] = false;
 		$section['interval_speed'] = 5000;
 		return $section;
 	}
 
 
-	/**
-	 * @static
-	 *
-	 */
 	static function SaveSection($return, $section, $type){
-		global $page;
-
 		if( !self::ContentKeyMatch($type) ){
 			return $return;
 		}
+		global $page;
 
 		$_POST += array('auto_start'=>'','images'=>array());
 
 		$page->file_sections[$section]['auto_start']		= ($_POST['auto_start'] == 'true');
+		$page->file_sections[$section]['alt_style']			= ($_POST['alt_style'] == 'true');
 		$page->file_sections[$section]['images']			= $_POST['images'];
 		$page->file_sections[$section]['captions']			= $_POST['captions'];
 		$page->file_sections[$section]['height']			= $_POST['height'];
@@ -203,13 +208,9 @@ class TwitterCarousel{
 
 	/**
 	 * Make sure the .js and .css is available to admins
-	 *
-	 * @static
-	 *
 	 */
 	static function GenerateContent_Admin(){
 		global $addonFolderName, $page, $addonRelativeCode;
-		//$page->head_script .= "\nvar SLIDESHOW_BASE = '".$addonRelativeCode."';\n";
 		self::AddComponents();
 	}
 
@@ -229,40 +230,20 @@ class TwitterCarousel{
 			common::LoadComponents( 'bootstrap-carousel' );
 		}
 		}
-
-		$page->head_js[] = '/data/_addoncode/'.$addonFolderName.'/jquery.mobile.custom.js';
-		$page->head_js[] = '/data/_addoncode/'.$addonFolderName.'/carousel.js';
-		$page->css_user[] = '/data/_addoncode/'.$addonFolderName.'/carousel.css';
-
 		$done = true;
 	}
 
 
-	/**
-	 *
-	 *
-	 */
 	static function InlineEdit_Scripts($scripts,$type){
-		global $addonRelativeCode;
-
 		if( !self::ContentKeyMatch($type) ){
 			return $scripts;
 		}
-
+		global $addonRelativeCode;
 		$scripts[] = '/include/js/inline_edit/image_common.js';
 		$scripts[] = '/include/js/inline_edit/gallery_edit_202.js';
 		$scripts[] = $addonRelativeCode.'/gallery_options.js';
 		$scripts[] = '/include/js/jquery.auto_upload.js';
 
-
 		return $scripts;
 	}
 }
-
-
-
-
-
-
-
-
--- gallery_options.js
+++ gallery_options.js
@@ -1,66 +1,191 @@
-$.extend(gp_editor, {sortable_area_sel:".carousel-indicators", img_name:"", img_rel:"", edit_links_target:".carousel-inner .carousel-item", make_sortable:!1, auto_start:!0, wake:function() {
-  $(gp_editor.edit_div).find(".gp_twitter_carousel").carousel("pause");
-}, sleep:function() {
-  0 < $("#ckeditor_wrap input[name=auto_start]:checked").length && $(gp_editor.edit_div).find(".gp_twitter_carousel").carousel("cycle");
-}, addedImage:function(a) {
-  var b = a.find("a:first").attr("href"), c = a.closest(".gp_twitter_carousel"), d = c.find(".carousel-inner"), e = $(".gp_blank_img").data("src") || "", b = $('<div class="carousel-item"><img src="' + e + '" style="background-image:url(' + b + ')"><div class="caption carousel-caption no_caption"></div></div>').appendTo(d);
-  a.attr("data-target", "#" + c.attr("id")).attr("data-slide-to", a.siblings().length);
-  b.siblings().length || (b.addClass("active"), a.addClass("active"));
-}, updateCaption:function(a, b) {
-  var c = $(a).find(".caption");
-  test = b.replace(/^\s+/, "");
-  "" == b ? c.addClass("no_caption") : c.removeClass("no_caption");
-}, removeImage:function(a) {
-  a = $(a).index();
-  var b = $(".gp_editing .carousel-indicators");
-  b.children().eq(a).remove();
-  $(".gp_editing").carousel("next");
-  b.children().each(function(a) {
-    $(this).attr("data-slide-to", a).data("slide-to", a);
-  });
-}, heightChanged:function() {
-  $(".gp_editing .gp_twitter_carousel").stop(!0, !0).delay(800).animate({"padding-bottom":this.value});
-}, intervalSpeed:function() {
-}, sortStop:function() {
-  var a = gp_editor.edit_div.find(".carousel-inner"), b = a.find(".carousel-item");
-  gp_editor.edit_div.find(gp_editor.sortable_area_sel).children().each(function(a) {
-    var d = $(this), e = d.data("slide-to");
-    b.eq(e).attr("data-new-position", a);
-    d.data("slide-to", a).attr("data-slide-to", a);
-  });
-  b.sort(function(a, b) {
-    return $(a).attr("data-new-position") < $(b).attr("data-new-position");
-  }).each(function() {
-    a.prepend(this);
-  });
-}, moveBack:function() {
-  var a = $(".gp_editing .carousel-inner .active"), b = a.prev();
-  if (b.length) {
-    var c = b.html(), d = a.html();
-    b.html(d);
-    a.html(c);
-    c = $(".gp_editing .carousel-indicators li");
-    b = c.eq(b.index());
-    a = c.eq(a.index());
-    c = b.html();
-    d = a.html();
-    b.html(d);
-    a.html(c);
-    $(".gp_editing").carousel("prev");
-  }
-}, moveForward:function() {
-  var a = $(".gp_editing .carousel-inner .active"), b = a.next();
-  if (b.length) {
-    var c = b.html(), d = a.html();
-    b.html(d);
-    a.html(c);
-    c = $(".gp_editing .carousel-indicators li");
-    b = c.eq(b.index());
-    a = c.eq(a.index());
-    c = b.html();
-    d = a.html();
-    b.html(d);
-    a.html(c);
-    $(".gp_editing").carousel("next");
-  }
-}});
+$.extend(gp_editor,{
+
+	sortable_area_sel:	'.carousel-indicators',
+	img_name:			'',
+	img_rel:			'',
+	edit_links_target:	'.carousel-inner .item, .carousel-inner .carousel-item',
+
+	make_sortable:		false,
+	auto_start:			true,
+
+	/**
+	 * Stop carousel cycling when editor is active
+	 */
+	wake: function(){
+		$(gp_editor.edit_div).find('.gp_twitter_carousel').carousel('pause');
+	},
+
+	/**
+	 * Restart carousel cycling if auto_start field is checked
+	 *
+	 */
+	sleep: function(){
+		if( $('#ckeditor_wrap input[name=auto_start]:checked').length > 0 ){
+			$(gp_editor.edit_div).find('.gp_twitter_carousel').carousel('cycle');
+		}
+	},
+
+
+	/**
+	 * Listen for an image being added
+	 *
+	 */
+	addedImage: function($li){
+		var src			= $li.find('a:first').attr('href');
+		var carousel	= $li.closest('.gp_twitter_carousel');
+		var $inner		= carousel.find('.carousel-inner');
+		var blank		= $('.gp_blank_img').data('src') || '';
+		var $item		= $('<div class="item carousel-item"><img src="'+blank+'" style="background-image:url('+src+')"><div class="caption carousel-caption no_caption"></div></div>').appendTo($inner);
+
+		$li.attr('data-target','#'+carousel.attr('id')).attr('data-slide-to',$li.siblings().length);
+
+		if( !$item.siblings().length ){
+			$item.addClass('active');
+			$li.addClass('active');
+		}
+
+	},
+
+
+	/**
+	 * Update the caption if the current image is the active image
+	 *
+	 */
+	updateCaption : function(current_image,text){
+		var caption_div = $(current_image).find('.caption');
+		test = text.replace(/^\s+/,'');
+		if( text == '' ){
+			caption_div.addClass('no_caption');
+		}else{
+			caption_div.removeClass('no_caption');
+		}
+	},
+
+
+
+	/**
+	 * Remove an image
+	 * Reset data-slide-to
+	 */
+	removeImage : function(current_image){
+
+		var index				= $(current_image).index();
+		var $indicator_area		= $('.gp_editing .carousel-indicators');
+
+		//remove indicator
+		$indicator_area.children().eq(index).remove();
+
+		$('.gp_editing').carousel('next');
+
+		//reorder indicatorss
+		$indicator_area.children().each(function(i){
+			$(this).attr('data-slide-to',i).data('slide-to',i);
+		});
+	},
+
+
+	/**
+	 * Size Changes
+	 *
+	 */
+	heightChanged : function(){
+		$('.gp_editing .gp_twitter_carousel').stop(true,true).delay(800).animate({'padding-bottom':this.value});
+	},
+
+
+	/**
+	 * Interval Speed
+	 *
+	 */
+	intervalSpeed : function(){
+	},
+
+
+	/**
+	 * Reorder full size images
+	 *
+	 */
+	sortStop: function(){
+
+		var full_wrap	= gp_editor.edit_div.find('.carousel-inner');
+		var full_images = full_wrap.find('.item','.carousel-item');
+
+
+		gp_editor.edit_div.find( gp_editor.sortable_area_sel ).children().each(function(i){
+			var $this		= $(this);
+			var slide_to	= $this.data('slide-to');
+
+			full_images.eq(slide_to).attr('data-new-position',i)
+
+			$this.data('slide-to',i).attr('data-slide-to',i);
+
+			var $img = $this;
+		});
+
+
+		//order the full images
+		full_images.sort(function(a,b){
+			return $(a).attr('data-new-position') < $(b).attr('data-new-position');
+		}).each(function(){
+			full_wrap.prepend(this);
+		});
+
+	},
+
+
+	/**
+	 * Move Back/Forward
+	 * @todo Remove, not used since Typesetter 5.0-rc3
+	 *
+	 */
+	moveBack : function(){
+		var $curr = $('.gp_editing .carousel-inner .active');
+		var $prev = $curr.prev();
+		if( !$prev.length ) return;
+
+		// swap images
+		var prev_html = $prev.html();
+		var curr_html = $curr.html();
+		$prev.html(curr_html);
+		$curr.html(prev_html);
+
+		//swap icons
+		var $indicators = $('.gp_editing .carousel-indicators li');
+		$prev = $indicators.eq( $prev.index() );
+		$curr = $indicators.eq( $curr.index() );
+		prev_html = $prev.html();
+		curr_html = $curr.html();
+		$prev.html(curr_html);
+		$curr.html(prev_html);
+
+		//move to previous image
+		$('.gp_editing').carousel('prev');
+
+	},
+
+	moveForward : function(){
+
+		var $curr = $('.gp_editing .carousel-inner .active');
+		var $next = $curr.next();
+		if( !$next.length ) return;
+
+
+		// swap images
+		var next_html = $next.html();
+		var curr_html = $curr.html();
+		$next.html(curr_html);
+		$curr.html(next_html);
+
+		//swap icons
+		var $indicators = $('.gp_editing .carousel-indicators li');
+		$next = $indicators.eq( $next.index() );
+		$curr = $indicators.eq( $curr.index() );
+		next_html = $next.html();
+		curr_html = $curr.html();
+		$next.html(curr_html);
+		$curr.html(next_html);
+
+		$('.gp_editing').carousel('next');
+	}
+
+});
